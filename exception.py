37e8a04a4f821da80930a71c97a865d3f24a8a4d
abhinav7876 <42429257+abhinav7876@users.noreply.github.com>
2025-12-29 02:38:08 +0530

    Refactor CustomException class for clarity

--- exception.py
+++ exception.py
@@ -3,9 +3,9 @@ from logger import logging
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)  #inhertance from exception class
+        super().__init__(error_message) 
         self.error_message=get_error_message(error_message,error_detail=error_detail)
-    def __str__(self):    #when we print then this error message will return
+    def __str__(self): 
         return self.error_message
 def get_error_message(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
